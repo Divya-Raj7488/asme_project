11a97283d8d0f50cdd1f01802b528941a1f8d380
Divya Raj <divyaraj7612@gmail.com>
2023-03-28 23:30:58 +0530

    mainpage created

--- src/cards/Cards.js
+++ src/cards/Cards.js
@@ -4,17 +4,6 @@ import "./Cards.css";
 const Cards = (props) => {
   return (
     <div className="cardsContainer" key={props.Id}>
-      {/* 
-      title
-     pubDate
-     link 
-     guid 
-     author 
-     thumbnail 
-     description 
-     content 
-     enclosure
-     categories  */}
       <h3>{props.title}</h3>
       <img src={props.Img} alt='newspaper' className="newspaperImg" />
       <p>{props.description}</p>
@@ -23,4 +12,40 @@ const Cards = (props) => {
   );
 }
 
+const Article = (props) => {
+  return (
+    <div className="mainpageArticleContainer" key={props.id}>
+              <div className="articlesContainer">
+                <h2
+                  style={{
+                    color: "white",
+                  }}
+                >
+                  {/*  */}
+                  {props.MainpageTitle}
+                </h2>
+                <p
+                  style={{
+                    color: "white",
+                  }}
+                >
+                  {props.mainpageDescription}
+                </p>
+              </div>
+              <div className="articleImg">
+                <img
+                  // src=
+                  src={props.ImgLink}
+                  alt="hello"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "1rem",
+                  }}
+                />
+              </div>
+            </div>
+  );
+}
+export {Article}
 export default Cards;
--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,42 +1,42 @@
 import React from "react";
 import axios from "axios";
 import "./Mainpage.css";
-import { useState } from "react";
+import { Article } from "../cards/Cards";
+import { useState, useEffect } from "react";
 
 export default function Mainpage() {
   const [TopStories, setTopStories] = useState([]);
 
-  axios
-    .get(
-      " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftimesofindia.indiatimes.com%2Frssfeedstopstories.cms"
-    )
-    .then((res) => {
-      const StoriesData = res.data;
-      const StoriesItems = StoriesData.items;
-      setTopStories(StoriesItems);
-    });
-    console.log(typeof(TopStories))
+  useEffect(() => {
+    axios
+      .get(
+        " https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Ftimesofindia.indiatimes.com%2Frssfeedstopstories.cms"
+      )
+      .then((res) => {
+        const Data = res.data.items;
+        setTopStories(Data);
+        console.log(Data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
+  console.log(TopStories);
+
   return (
     <div className="mainpageContainer">
-      <div className="mainpageArticleContainer">
-        <div className="articlesContainer">
-          <h2
-            style={{
-              color: "white",
-            }}
-          >
-            {TopStories.title}
-          </h2>
-          <p
-            style={{
-              color: "white",
-            }}
-          >
-            description
-          </p>
-        </div>
-        <div className="articleImg"></div>
-      </div>
+      {TopStories &&
+        TopStories.map((story) => {
+          return (
+            <Article
+            id= {story.id}
+            MainpageTitle={story.title}
+            mainpageDescription={story.description}
+            ImgLink={story.enclosure.link}
+            />
+          );
+        })}
     </div>
   );
 }
